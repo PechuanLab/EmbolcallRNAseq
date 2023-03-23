e1a5f84429197b8ee89707474a4a646f1642244a
Pechuan Jorge, Ximo {GRMW~South San Francisco} <pechuan-jorge.joaquin@gene.com>
2023-03-22 17:30:56 -0700

    Fixed Projection score Plot

--- R/ProjectionScorePlot.R
+++ R/ProjectionScorePlot.R
@@ -15,16 +15,8 @@ ProjectionScorePlot <- function(ps.df){
   Y =  predict(spl,newdata=X) # calculate predictions for that sequence
   TagetTheta = Y$x[which.max(Y$y)]
   
-  
-  
-  lines(X$Thetas,Y,type="l",main="Original fit") # check if predictions are correct
-  
-  dY <- diff(Y)/diff(X$Thetas)  # calculate the dY values for our function
-  dX <- rowMeans(embed(X$Thetas,2)) # centers the X values for plotting
-  plot(dX,dY,type="l",main="Derivative") # check if derivative is correct
-  deriv=predict(spl, deriv=1,newdata = c(0))
   # Using ggscatter
-  ggscatter(ps.df, x = "Thetas", y = "ProjectionScores")+geom_vline(xintercept = TagetTheta)+
+  ggpubr::ggscatter(ps.df, x = "Thetas", y = "ProjectionScores")+geom_vline(xintercept = TagetTheta)+
     geom_line(aes(Y$x,Y$y))+xlab(TeX("$\\theta$"))+ylab("Projection Score")+
     annotate("text", x = TagetTheta+0.025, y = 0.2, label = TagetTheta)+ theme_linedraw()+labs_pubr()
     
