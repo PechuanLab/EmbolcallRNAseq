da19cfbb65a376896569c2bb9bf9226d75838f82
Pechuan Jorge, Ximo {GRMW~South San Francisco} <pechuan-jorge.joaquin@gene.com>
2023-03-23 20:12:11 -0700

    GSEA PCA

---
+++ R/GseaPCA.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param PCAToolsObject
+#' @param npcs
+#' @param species
+#' @return GSEA database
+#' @export
+#'
+#' @examples  GseaPCA( )
+
+GseaPCA <- function(PCAToolsObject, npcs = 3, species){
+  
+  # Get the PCA loadings matrix
+  loadings_mat = p$loadings
+  
+  for (i in 1:npcs) {
+    # Prepare stats
+    pc = data.frame(symbol = rownames(loadings_mat),logFC = loadings_mat[,i],adj.P.Val = 0)
+    stats = GSEAPrepare(signature = pc , Foldstatistic = "logFC",species)
+    ClusterProfilerOnthologies(stats,species,Prefix = paste0("PC",i),contraste =  paste0("PC",i)) 
+  }
+}
+
