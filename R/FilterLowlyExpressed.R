15aa179169a8b807efb760b6ddd8cef758d4bc95
Ximo Pechuan i Jorge <lionzord@gmail.com>
2023-05-30 14:14:37 -0700

    Update FilterLowlyExpressed.R

--- R/FilterLowlyExpressed.R
+++ R/FilterLowlyExpressed.R
@@ -8,10 +8,10 @@
 #'
 #' @examples FilterLowlyExpressed(y.all,nsamples = 3)
 #' 
-FilterLowlyExpressed <- function(y.all,nsamples = 3){
+FilterLowlyExpressed <- function(y.all,nsamples = 3,nreads = 15){
   # Filter lowly expressed genes
   libsize = mean(y.all$samples$lib.size)
-  cpmthresh = 15/libsize*10^6
+  cpmthresh = nreads/libsize*10^6
   keep.exprs = rowSums(edgeR::cpm(y.all$counts) >= cpmthresh) > nsamples
   yf =  y.all[keep.exprs,, keep.lib.sizes=FALSE]
   return(yf)
