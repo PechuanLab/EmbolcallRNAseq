9ab6b55749ba10c6075d17d3834ca2da0c2d731a
Ximo Pechuan i Jorge <lionzord@gmail.com>
2023-10-16 16:01:19 -0700

    Update AlphaPE.R
    better documented

--- R/AlphaPE.R
+++ R/AlphaPE.R
@@ -1,17 +1,17 @@
-#' Title
+#' Alpha from the projection score calculation. 
+#' Calculates the fraction of the variance explained by the chosen principal components and moderates it by taking the square root.
+#' @param DataMatrix. Dataset in matrix form.
+#' @param NPCs. Number of principal components that are considered informative for the score.
 #'
-#' @param DataMatrix
-#' @param NPCs
-#'
-#' @return Fraction of variance for projection score
+#' @return alphaFinal. Fraction of variance explained by the principal components for the projection score calculation.
 #' @export
 #'
-#' @examples AlphaPE()
+#' @examples AlphaPE(DataMatrix, NPCs = 3)
 #' 
 AlphaPE <- function(DataMatrix,NPCs =  3){
-  # svd
+  # Calculate the svd
   svdRes = svd(DataMatrix)
-  # Rasmuss Hennigson Talus Plot
+  # Take Eigenvalues to estimate fraction of variance step.
   lambdas= (svdRes$d)^2
   alphak = sum(lambdas[1:NPCs])
   alphaFinal = (alphak/sum(lambdas))^(1/2)
