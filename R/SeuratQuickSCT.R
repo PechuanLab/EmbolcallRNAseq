ae02509a236a2242bb4078db770e12e57ab93a78
jopejor <pechuanjorge@revmed.com>
2025-03-12 00:28:23 +0100

    Quick SCT clustering

---
+++ R/SeuratQuickSCT.R
@@ -0,0 +1,49 @@
+#' Process Seurat Object with SCTransform, PCA, and UMAP
+#'
+#' This function processes a Seurat object by normalizing the data, scoring cell cycles, 
+#' applying SCTransform, running PCA, finding neighbors, running UMAP, and finding clusters 
+#' at multiple resolutions.
+#'
+#' @param seu A Seurat object to be processed.
+#' @param ndims An integer specifying the number of dimensions to use for PCA and UMAP. Default is 30.
+#'
+#' @return A processed Seurat object with normalized data, cell cycle scores, PCA, UMAP, and clusters.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   # Assuming 'seurat_object' is your Seurat object
+#'   seurat_object <- SeuratQuickSCT(seurat_object, ndims = 30)
+#' }
+SeuratQuickSCT <- function(seu, ndims = 30) {
+  # Normalize data
+  seu <- NormalizeData(seu, scale.factor = median(seu@meta.data$nCount_RNA))
+  
+  # Cell cycle scoring
+  s.genes <- cc.genes$s.genes %>% tolower() %>% firstup()
+  g2m.genes <- cc.genes$g2m.genes %>% tolower() %>% firstup()
+  seu <- CellCycleScoring(seu, s.features = s.genes, g2m.features = g2m.genes, set.ident = FALSE)
+  
+  # SCTransform
+  seu <- SCTransform(seu, verbose = TRUE, conserve.memory = TRUE, 
+                     vars.to.regress = c("S.Score", "G2M.Score", "percent_ribo", "percent_mito"))
+  
+  # Run PCA
+  seu <- RunPCA(seu, npcs = 150)
+  
+  # Find neighbors and run UMAP
+  seu <- FindNeighbors(seu, reduction = "pca", dims = 1:ndims)
+  seu <- RunUMAP(seu, reduction = "pca", dims = 1:ndims)
+  
+  # Clean up metadata
+  seu@meta.data <- seu@meta.data[, !grepl(paste0("^", "SCT_"), colnames(seu@meta.data))]
+  
+  # Find clusters at multiple resolutions
+  resolutions <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.5, 2)
+  for (res in resolutions) {
+    seu <- FindClusters(seu, verbose = FALSE, resolution = res)
+  }
+  
+  return(seu)
+}
+
